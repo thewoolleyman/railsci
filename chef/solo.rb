438b9400c475a8cb8117d1a733e350e41be2f50d
Chad Woolley <thewoolleyman@gmail.com>
2010-04-22 23:40:49 -0700

    make path absolute

--- chef/solo.rb
+++ chef/solo.rb
@@ -1,2 +1,2 @@
 file_cache_path "/tmp/chef_solo"
-cookbook_path ["/tmp/chef_solo/opscode_cookbooks","tmp/chef_solo/railsci_chef_repo/site-cookbooks"]
+cookbook_path ["/tmp/chef_solo/opscode_cookbooks","/tmp/chef_solo/railsci_chef_repo/site-cookbooks"]
