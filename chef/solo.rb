2171cc6784eaba30dc774c9a6ad669e263dd8579
Chad Woolley <thewoolleyman@gmail.com>
2010-05-23 06:37:05 +0000

    update chef config files to point to custom railsci recipe

--- chef/solo.rb
+++ chef/solo.rb
@@ -1,2 +1,5 @@
 file_cache_path "/tmp/chef_solo"
+file_store_path "/tmp/chef_solo"
+log_level :info
+Chef::Log::Formatter.show_time = false
 cookbook_path ["/tmp/chef_solo/railsci_chef_repo/cookbooks/opscode","/tmp/chef_solo/railsci_chef_repo/site-cookbooks"]
