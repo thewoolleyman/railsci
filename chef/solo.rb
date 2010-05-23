a8ab541ef54584a39c6319d96a3669f3391d210f
Chad Woolley <thewoolleyman@gmail.com>
2010-05-23 21:29:26 +0000

    restructure site cookbook

--- chef/solo.rb
+++ chef/solo.rb
@@ -3,4 +3,4 @@ file_cache_path chef_dir
 file_store_path chef_dir
 log_level :info
 Chef::Log::Formatter.show_time = false
-cookbook_path [File.join(chef_dir,"railsci_chef_repo/cookbooks/opscode"),File.join(chef_dir,"railsci_chef_repo/site_cookbooks/railsci")]
+cookbook_path [File.join(chef_dir,"railsci_chef_repo/cookbooks/opscode"),File.join(chef_dir,"railsci_chef_repo/site-cookbooks/railsci")]
