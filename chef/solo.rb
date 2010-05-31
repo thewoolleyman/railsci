d6563bf20e98da32246f27437c1427878cfccd28
Chad Woolley <thewoolleyman@gmail.com>
2010-05-31 07:12:12 +0000

    reformat

--- chef/solo.rb
+++ chef/solo.rb
@@ -3,4 +3,7 @@ file_cache_path chef_dir
 file_store_path chef_dir
 log_level :info
 Chef::Log::Formatter.show_time = false
-cookbook_path [File.join(chef_dir,"railsci_chef_repo/cookbooks/opscode"),File.join(chef_dir,"railsci_chef_repo/site-cookbooks/railsci")]
+cookbook_path [
+  File.join(chef_dir,"railsci_chef_repo/cookbooks/opscode"),
+  File.join(chef_dir,"railsci_chef_repo/site-cookbooks/railsci")
+]
