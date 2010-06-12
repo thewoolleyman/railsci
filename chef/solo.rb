ba14bfef43201bb6c97203ed27a4d442cfca1c9b
Chad Woolley <thewoolleyman@gmail.com>
2010-06-12 16:07:14 +0000

    update node.json, add teamcity user and railsci_post_setup

--- chef/solo.rb
+++ chef/solo.rb
@@ -1,7 +1,7 @@
 chef_dir ||= ENV["RAILSCI_CHEF_DIR"] || File.join(ENV["HOME"],'chef')
 file_cache_path chef_dir
 file_store_path chef_dir
-log_level :info
+log_level :debug
 Chef::Log::Formatter.show_time = false
 cookbook_path [
   File.join(chef_dir,"railsci_chef_repo/cookbooks/opscode"),
